0865108c21d5ecbe1c5add07e8f1ee958e910501
Default User <laurence.a.maddox@gmail.com>
2017-09-22 17:43:24 -0500

    backup

--- src/zepast.c
+++ src/zepast.c
@@ -7,30 +7,6 @@
 
 #include <zepast.h>
 
-TODO (break out to separate source file)
-
-__attribute__ ((leaf, nonnull (1, 3), nothrow))
-void stats (
-   stats_t s[],      size_t nstats,
-   unigram_t vals[], size_t nval) {
-   size_t si, vi;
-	#pragma GCC diagnostic push
-	#pragma GCC diagnostic ignored "-Wtraditional-conversion"
-   /* presumably si loop can be parallelized */
-   for (si = 0; si != nstats; si++)
-      s[si].init (s[si].stats, nval);
-   /* vi loop cannot necessarily be parallelized,
-    * because diffs() */
-   for (vi = 0; vi != nval; vi++)
-      for (si = 0; si != nstats; si++)
-         s[si].update (s[si].stats, vals[vi], nval);
-   for (si = 0; si != nstats; si++)
-      s[si].finish (s[si].stats, nval);
-	#pragma GCC diagnostic pop
-}
-
-TODO (stats2() fully parallelize-able)
-
 __attribute__ ((leaf, nonnull (1, 3), nothrow))
 void zepast (
    zepast_t z[],     size_t nzepast,
