a96663f774daf6a2c41002b8d26b953cc9db95f2
Default User <laurence.a.maddox@gmail.com>
2017-09-22 22:41:45 -0500

    backup

--- src/stat-test.c
+++ src/stat-test.c
@@ -12,13 +12,13 @@
 
 #include <stat.h>
 #include <mean.h>
-#include <variance.h>
+/*#include <variance.h>*/
 
 __attribute__ ((nothrow, warn_unused_result))
 int main (void) {
    stat_t s[2];
    mean_t mean;
-   variance_t variance;
+   /*variance_t variance;*/
    char const str[] = "Hello, World!";
    unigram_t vals[sizeof (str) * sizeof (char)];
    size_t vi;
@@ -34,16 +34,16 @@ int main (void) {
    s[0].stat   = &mean;
    ez_stat (s + 0, vals, ARRSZ (vals));
 
-   variance.ct = mean.res;
+   /*variance.ct = mean.res;
    s[1].init   = init_variance;
    s[1].update = update_mean;
    s[1].finish = finish_mean;
    s[1].stat   = &variance;
-   ez_stat (s + 1, vals, ARRSZ (vals));
+   ez_stat (s + 1, vals, ARRSZ (vals));*/
 
    (void) printf ("sum:%d\n",      (int) (mean.sum));
    (void) printf ("mean:%g\n",     mean.res);
-   (void) printf ("variance:%g\n", variance.sum);
-   (void) printf ("stddev:%g\n",   variance.res);
+   /*(void) printf ("variance:%g\n", variance.sum);
+   (void) printf ("stddev:%g\n",   variance.res);*/
    return EXIT_SUCCESS;
 }
